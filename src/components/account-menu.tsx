cb54acf275af7ca45806592baf428235ba19f548
KicungaTI <kicungati@gmail.com>
2024-12-02 19:20:11 +0100

    ThemeToggle e menu da conta

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import { DropdownMenuContent } from '@radix-ui/react-dropdown-menu'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="mt-4 w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span className="text-sm">João Kicunga</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            Kicungati@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Building className="mr-0 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-0 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
