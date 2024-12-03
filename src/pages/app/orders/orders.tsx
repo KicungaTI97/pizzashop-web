c7837b48b96c70d30cb0ec0508da3b964a9ea7ff
KicungaTI <kicungati@gmail.com>
2024-12-03 07:10:12 +0100

    Componente: Filtros de pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,16 +1,15 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
+import { OrderTableRow } from './order.table-row'
+import { OrderTableFilters } from './order-table-filters'
 
 export function Orders() {
   return (
@@ -20,14 +19,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros:</span>
-          <Input
-            placeholder="Nome do cliente"
-            type="text"
-            className="h-8 w-[320px]"
-          />
-        </form>
+        <OrderTableFilters />
         <div className="ronded-md border">
           <Table>
             <TableHeader>
@@ -42,50 +34,11 @@ export function Orders() {
                 <TableHead className="w-[132px]"></TableHead>
               </TableRow>
             </TableHeader>
-            {Array.from({ length: 10 }).map((_, i) => {
-              return (
-                <TableBody key={i}>
-                  <TableRow>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className="h-3 w-3" />
-                        <label className="sr-only">Detalhes do pedido</label>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="font-mono text-xs font-medium">
-                      21rfewfjklwe48wedfqty
-                    </TableCell>
-                    <TableCell className="text-muted-foreground">
-                      há 20 minutos
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="font-medium">
-                      João Manuel Kicunga
-                    </TableCell>
-                    <TableCell className="font-medium">250.000.00kz</TableCell>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <ArrowRight className="mr-2 h-2 w-3" />
-                        Aprovar
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-2 h-2 w-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              )
-            })}
+            <TableBody>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <OrderTableRow key={i} />
+              })}
+            </TableBody>
           </Table>
         </div>
       </div>
